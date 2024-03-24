a824dd26835209115523fe2be3f7223cb2e42bf6
Mehran Alidoost Nia <24442175+alidoostnia@users.noreply.github.com>
2024-03-25 03:22:36 +0330

    Initialize the Repo

---
+++ Logger/src/com/android/Logger/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.android.Logger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.Logger.R
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_white_page)
+    }
+}
